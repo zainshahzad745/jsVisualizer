12d88fc0f66b983186db5b0b6896ccdb67b3818b
zainshahzad745 <zainshahza@gmail.com>
2024-09-14 17:27:58 +0500

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-import Image from "next/image";
-import EventLoop from "./components/EventLoop";
 import EventLoopVisualizer from "./components/EventLoopNew";
-import EventLoopPage from "./components/EventLoopPage";
 export default function Home() {
   return (
     <div>
-      {/* <EventLoopPage /> */}
       <EventLoopVisualizer />
     </div>
   );
